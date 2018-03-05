eb730fdeff51982daa0b9cd4a2777194db4cbe1d
Mauricio IC <mauricio_ic19@outlook.com>
2018-03-05 11:07:05 -0600

    Structure changes

--- php/connection.php
+++ php/connection.php
@@ -2,15 +2,14 @@
 	class DB{
 	  public function connect(){
 	    $database = "pareto";
+	    $host = "osorniovelazquez.ml";
 	    $user = "graphy";
-	    $host = "https://osorniovelazquez.ml";
 	    $pass = "graphy123";
 
 	    $conn = new mysqli($host, $user, $pass, $database);
 	    if ($conn->connect_errno > 0) {
 	      echo $conn->error."</br>";
 	    }
-	    echo 1;
 	    return $conn;
 	  }
 	}
--- php/prueba.php
+++ php/prueba.php
@@ -1,4 +1,6 @@
-<?php  
+<?php
+  require_once('connection.php');
+  
   $conn = new DB();
   $conn = $conn->connect();
 
