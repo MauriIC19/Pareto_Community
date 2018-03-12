bca43c08fb34c8175dc231d8366d9db1503342f8
Mauricio IC <mauricio_ic19@outlook.com>
2018-03-12 10:36:10 -0600

    Arreglo de funcionalidad y se añade lo de los sombreros.

---
+++ js/hats.js
@@ -0,0 +1,50 @@
+var hats = {
+	//Rojo
+	1: {
+		emocion1: 'Satisfacción',
+		emocion2: 'Esperanza'
+	},
+	//Blanco
+	2: {
+		emocion1: 'Información',
+		emocion2: 'Utilidad',
+	},
+	//Negro
+	3: {
+		emocion1: 'Juicio',
+		emocion2: 'Cautela',
+	},
+	//Amarillo
+	4: {
+		emocion1: 'Beneficios',
+		emocion2: 'Felicidad',
+	},
+	//Verde
+	5: {
+		emocion1: 'Creatividad',
+		emocion2: 'Innovación',
+	},
+	//Azul
+	6: {
+		emocion1: 'Control',
+		emocion2: 'Integridad',
+	}
+}
+
+function getSelectedHat(){
+	//var selectedHats = document.querySelectorAll('.selectedHat');
+	var selectedHats = ['Satisfacción', 'Juicio', 'Control'];
+	var incidencies = [0, 0, 0, 0, 0, 0];
+	var x = Object.keys(hats).length;
+
+	var id = 0;
+
+	for(var i = 1; i <= x; i++){
+		for (var j = 0; j < selectedHats.length; j++) {
+			if(hats[i].emocion1 == selectedHats[j] || hats[i].emocion2 == selectedHats[j]){
+				incidencies[i-1] = incidencies[i-1] + 1;
+			}
+		}
+	}
+	return incidencies.indexOf(Math.max.apply(Math, incidencies)) + 1;
+}
--- php/createProblems.php
+++ php/createProblems.php
@@ -10,32 +10,20 @@
   $autor = $_POST['autor'];
   $factores = explode(',', $factores);
 
-  $flagProblem = false;
-  $flagFactors = false;
-
   $query = "INSERT INTO Problema (nombreProblema, descripcion, autor) VALUES('{$nombreProblema}', '{$descripcion}', '{$autor}');";
 
   $conn->begin_transaction();
 
-  if ($conn->query($query)) {
-  	$flagProblem = true;
-  }
+  $conn->query($query);
 
   $idProblema = $conn->insert_id;
 
   for ($i = 0; $i < count($factores); $i++) { 
   	$query = "INSERT INTO Factores (nombreFactor, idProblema) VALUES('{$factores[$i]}', {$idProblema})";
-  	if ($conn->query($query)) {
-  		$flagFactors = true;
-  	}
-  	else {
-  		$flagFactors = false;
-  		break;
-  	}
+  	$conn->query($query);
   }
 
-  if ($flagProblem && $flagFactors) {
-  	$conn->commit();
+  if($conn->commit()){
   	echo 1;
   }
   else{
